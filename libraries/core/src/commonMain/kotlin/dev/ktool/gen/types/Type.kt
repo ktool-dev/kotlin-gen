048b0a0616f8bd41a659b391c417b52e1bfb6273
Aaron Freeman <aaron@freeman.zone>
2025-10-03 08:48:31 -0500

    feat: Adding ability to create type from KClass

--- libraries/core/src/commonMain/kotlin/dev/ktool/gen/types/Type.kt
+++ libraries/core/src/commonMain/kotlin/dev/ktool/gen/types/Type.kt
@@ -2,6 +2,7 @@ package dev.ktool.gen.types
 
 import dev.ktool.gen.CodeWriter
 import dev.ktool.gen.Writable
+import kotlin.reflect.KClass
 
 val IntType: Type get() = Type("Int")
 val FloatType: Type get() = Type("Float")
@@ -10,6 +11,17 @@ val StringType: Type get() = Type("String")
 val BooleanType: Type get() = Type("Boolean")
 val EnumType: Type get() = Type("Enum")
 val LongType: Type get() = Type("Long")
+val ShortType: Type get() = Type("Short")
+val ByteType: Type get() = Type("Byte")
+val CharType: Type get() = Type("Char")
+val IntRangeType: Type get() = Type("IntRange")
+val LongRangeType: Type get() = Type("LongRange")
+val ShortRangeType: Type get() = Type("IntRange")
+val ByteRangeType: Type get() = Type("IntRange")
+val CharRangeType: Type get() = Type("IntRange")
+val BooleanRangeType: Type get() = Type("Boolean")
+val EnumRangeType: Type get() = Type("Enum")
+val UnitType: Type get() = Type("Unit")
 
 class Type(
     var name: String,
@@ -78,3 +90,28 @@ interface SuperTypes {
         superTypes.add(Type(name, false, block))
     }
 }
+
+fun KClass<*>.createType(nullable: Boolean = false, typeArguments: List<Type> = listOf()): Type {
+    val packageName = qualifiedName?.substringBeforeLast('.')
+    val name = if (packageName in AUTO_IMPORTED_PACKAGES) {
+        simpleName
+    } else {
+        qualifiedName
+    } ?: error("Cannot create type from $this")
+    return Type(name, nullable, typeArguments)
+}
+
+private val AUTO_IMPORTED_PACKAGES = setOf(
+    "kotlin",
+    "kotlin.annotation",
+    "kotlin.collections",
+    "kotlin.comparisons",
+    "kotlin.coroutines",
+    "kotlin.io",
+    "kotlin.ranges",
+    "kotlin.sequences",
+    "kotin.text",
+    "kotlin.math",
+    "java.lang",
+    "kotlin.jvm",
+)
